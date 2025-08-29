ab6fd3235034c16c6c1895ef39854b03aaf90598
Amp&rsand <113919662+MuriloSchneiderS@users.noreply.github.com>
2025-08-28 21:52:15 -0300

    HospedeDAO

--- src/modelDAO/HospedeDAO.java
+++ src/modelDAO/HospedeDAO.java
@@ -1,17 +1,124 @@
 package modelDAO;
+
 import model.Hospede;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
-public class HospedeDAO implements InterfaceDAO<Hospede>{
+public class HospedeDAO implements InterfaceDAO<Hospede> {
 
     @Override
     public void Create(Hospede objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sqlInstrucao = "INSERT INTO hospede("
+                + " nome,"
+                + " fone,"
+                + " fone2,"
+                + " email,"
+                + " cep,"
+                + " logradouro,"
+                + " bairro,"
+                + " cidade,"
+                + " complemento,"
+                + " data_cadastro,"
+                + " cpf,"
+                + " rg,"
+                + " obs,"
+                + " status,"
+                + " razao_social,"
+                + " cnpj,"
+                + " inscricao_estadual,"
+                + " contato)"
+                + " VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        Connection conexao = ConnectionFactory.getConnection();
+        PreparedStatement pstm = null;
+        try {
+            pstm = conexao.prepareStatement(sqlInstrucao);
+            pstm.setString(1, objeto.getNome());
+            pstm.setString(2, objeto.getFone1());
+            pstm.setString(3, objeto.getFone2());
+            pstm.setString(4, objeto.getEmail());
+            pstm.setString(5, objeto.getCep());
+            pstm.setString(6, objeto.getLogradouro());
+            pstm.setString(7, objeto.getBairro());
+            pstm.setString(8, objeto.getCidade());
+            pstm.setString(9, objeto.getComplemento());
+            pstm.setNull(10, java.sql.Types.DATE);
+            pstm.setString(11, objeto.getCpf());
+            pstm.setString(12, objeto.getRg());
+            pstm.setString(13, objeto.getObs());
+            pstm.setString(14, String.valueOf(objeto.getStatus()));
+            pstm.setString(15, objeto.getRazaoSocial());
+            pstm.setString(16, objeto.getCnpj());
+            pstm.setString(17, objeto.getInscricaoEstadual());
+            pstm.setString(18, objeto.getContato());
+            pstm.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            ConnectionFactory.closeConnection(conexao, pstm);
+        }
     }
 
     @Override
     public Hospede Retrieve(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sqlInstrucao = "SELECT"
+                + " id,"
+                + " nome,"
+                + " fone,"
+                + " fone2,"
+                + " email,"
+                + " cep,"
+                + " logradouro,"
+                + " bairro,"
+                + " cidade,"
+                + " complemento,"
+                + " data_cadastro,"
+                + " cpf,"
+                + " rg,"
+                + " obs,"
+                + " status,"
+                + " razao_social,"
+                + " cnpj,"
+                + " inscricao_estadual,"
+                + " contato"
+                + " FROM hospede WHERE id=?";
+        Connection conexao = ConnectionFactory.getConnection();
+        PreparedStatement pstm = null;
+        ResultSet rst = null;
+        Hospede hospede = new Hospede();
+        try {
+            pstm = conexao.prepareStatement(sqlInstrucao);
+            pstm.setInt(1, id);
+            rst = pstm.executeQuery();
+            while (!rst.next()) {
+                hospede.setId(rst.getInt("id"));
+                hospede.setNome(rst.getString(2));
+                hospede.setFone1(rst.getString("fone"));
+                hospede.setFone2(rst.getString("fone2"));
+                hospede.setEmail(rst.getString("email"));
+                hospede.setCep(rst.getString("cep"));
+                hospede.setLogradouro(rst.getString("logradouro"));
+                hospede.setBairro(rst.getString("bairro"));
+                hospede.setCidade(rst.getString("cidade"));
+                hospede.setComplemento(rst.getString("complemento"));
+                hospede.setDataCadastro(String.valueOf(rst.getDate("data_cadastro")));
+                hospede.setCpf(rst.getString("cpf"));
+                hospede.setRg(rst.getString("rg"));
+                hospede.setObs(rst.getString("obs"));
+                hospede.setStatus(rst.getString("status").charAt(0));
+                hospede.setRazaoSocial(rst.getString("razao_social"));
+                hospede.setCnpj(rst.getString("cnpj"));
+                hospede.setInscricaoEstadual(rst.getString("inscricao_estadual"));
+                hospede.setContato(rst.getString("contato"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            ConnectionFactory.closeConnection(conexao, pstm, rst);
+            return hospede;
+        }
     }
 
     @Override
@@ -28,5 +135,5 @@ public class HospedeDAO implements InterfaceDAO<Hospede>{
     public void Delete(Hospede objeto) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
