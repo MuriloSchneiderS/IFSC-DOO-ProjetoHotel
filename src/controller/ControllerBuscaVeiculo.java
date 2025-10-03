c923b32386b25348a83dda8806a612e1b91e70bd
Amp&rsand <113919662+MuriloSchneiderS@users.noreply.github.com>
2025-10-02 21:59:31 -0300

    (em desenvolvimento) correcao busca Veiculo
    não finalizado

--- src/controller/ControllerBuscaVeiculo.java
+++ src/controller/ControllerBuscaVeiculo.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import model.Marca;
+import model.Modelo;
 import model.Veiculo;
 import view.TelaBuscaVeiculo;
 
@@ -25,58 +27,112 @@ public class ControllerBuscaVeiculo implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent evento) {
+
         //Botão Carregar
         if (evento.getSource() == this.telaBuscaVeiculo.getjButtonCarregar()) {
-            if(this.telaBuscaVeiculo.getjTableDados().getRowCount()==0){
-                JOptionPane.showMessageDialog(null,"Nenhum dado selecionado.");
-            }else{
+            if (this.telaBuscaVeiculo.getjTableDados().getRowCount() == 0) {
+                JOptionPane.showMessageDialog(null, "Nenhum dado selecionado.");
+            } else {
                 //retorno dos dados para a tela de cadastro
                 ControllerCadVeiculo.codigo = (int) this.telaBuscaVeiculo.getjTableDados().getValueAt(this.telaBuscaVeiculo.getjTableDados().getSelectedRow(), 0);
                 this.telaBuscaVeiculo.dispose();
             }
-        //Botão Filtrar
+
+            //Botão Filtrar
         } else if (evento.getSource() == this.telaBuscaVeiculo.getjButtonFiltar()) {
             if (this.telaBuscaVeiculo.getjTFFiltro().getText().trim().equalsIgnoreCase("")) {
                 JOptionPane.showMessageDialog(null, "Sem Dados para a Seleção...");
             } else {
-                if (this.telaBuscaVeiculo.getjCBFiltro().getSelectedIndex() == 0) {//Ordenar por Id
-                    //Criando objeto para receber o dado que virà do banco de dados
-                    Veiculo veiculo = new Veiculo();
-                    //Carregando o registro do veiculo na entidade para o objeto veiculo
-                    veiculo = service.VeiculoService.Carregar(Integer.parseInt(this.telaBuscaVeiculo.getjTFFiltro().getText()));
-                    //Criando um objeto tabela do tipo defaulttableModel e atribuindo o veiculo da tela a ele
-                    DefaultTableModel tabela = (DefaultTableModel) this.telaBuscaVeiculo.getjTableDados().getModel();
-                    tabela.addRow(new Object[]{
-                        veiculo.getId(), 
-                        veiculo.getPlaca(), 
-                        veiculo.getCor(), 
-                        veiculo.getModelo().getDescricao(), 
-                        veiculo.getFuncionario().getNome(), 
-                        veiculo.getFornecedor().getNome(), 
-                        veiculo.getHospede().getNome()
-                    });
-                } else if (this.telaBuscaVeiculo.getjCBFiltro().getSelectedIndex() == 1) {//Ordenar por Descricao
-                    //Criando a lista para receber as veiculos
-                    List<Veiculo> listaVeiculos = new ArrayList<>();
-                    //Criando um objeto tabela do tipo defaulttablemodel e atribuindo o veiculo da tabela a ele
-                    listaVeiculos = service.VeiculoService.Carregar("nome", this.telaBuscaVeiculo.getjTFFiltro().getText());
-                    DefaultTableModel tabela = (DefaultTableModel) this.telaBuscaVeiculo.getjTableDados().getModel();
-                    tabela.setRowCount(0);
-                    //Adicionando os veiculos na tabela
-                    for(Veiculo veiculoAtualDaLista : listaVeiculos){
+                DefaultTableModel tabela = (DefaultTableModel) this.telaBuscaVeiculo.getjTableDados().getModel();
+                tabela.setRowCount(0); // Reseta a tabela
+
+                switch (this.telaBuscaVeiculo.getjCBFiltro().getSelectedIndex()) {
+                    case 0://Id
+                    {
+                        //Criando objeto para receber o dado que virà do banco de dados
+                        Veiculo veiculo = new Veiculo();
+                        Modelo modelo = new Modelo();
+                        //Carregando o registro do veiculo na entidade para o objeto veiculo
+                        veiculo = service.VeiculoService.Carregar(Integer.parseInt(this.telaBuscaVeiculo.getjTFFiltro().getText()));
+                        modelo = service.ModeloService.Carregar(veiculo.getModelo().getId());
+                        //Criando um objeto tabela do tipo defaulttableModel e atribuindo o veiculo da tela a ele
+                        tabela = (DefaultTableModel) this.telaBuscaVeiculo.getjTableDados().getModel();
                         tabela.addRow(new Object[]{
-                            veiculoAtualDaLista.getId(), 
-                            veiculoAtualDaLista.getPlaca(), 
-                            veiculoAtualDaLista.getCor(), 
-                            veiculoAtualDaLista.getModelo().getDescricao(), 
-                            veiculoAtualDaLista.getFuncionario().getNome(), 
-                            veiculoAtualDaLista.getFornecedor().getNome(), 
-                            veiculoAtualDaLista.getHospede().getNome()
+                            veiculo.getId(),
+                            veiculo.getPlaca(),
+                            veiculo.getCor(),
+                            service.MarcaService.Carregar(modelo.getMarca().getId()).getDescricao(),
+                            modelo.getDescricao()
                         });
+                        break;
                     }
+                    case 1://Placa
+                    {
+                        //Criando a lista para receber os veiculos
+                        List<Veiculo> listaVeiculos = new ArrayList<>();
+                        Modelo modelo = new Modelo();
+                        //Criando um objeto tabela do tipo defaulttablemodel e atribuindo o veiculo da tabela a ele
+                        listaVeiculos = service.VeiculoService.Carregar("placa", this.telaBuscaVeiculo.getjTFFiltro().getText());
+                        tabela = (DefaultTableModel) this.telaBuscaVeiculo.getjTableDados().getModel();
+                        tabela.setRowCount(0);
+                        //Adicionando os veiculos na tabela
+                        for (Veiculo veiculoAtualDaLista : listaVeiculos) {
+                            modelo = service.ModeloService.Carregar(veiculoAtualDaLista.getModelo().getId());
+                            tabela.addRow(new Object[]{
+                                veiculoAtualDaLista.getId(),
+                                veiculoAtualDaLista.getPlaca(),
+                                veiculoAtualDaLista.getCor(),
+                                service.MarcaService.Carregar(modelo.getMarca().getId()).getDescricao(),
+                                modelo.getDescricao()
+                            });
+                        }
+                        break;
+                    }
+                    case 2://Cor
+                        
+                        break;
+                    case 3://Marca
+                        List<Veiculo> listaVeiculos = new ArrayList<>();
+                        Marca marca = service.MarcaService.Carregar("descricao", this.telaBuscaVeiculo.getjTFFiltro().getText()).getFirst();
+
+                        listaVeiculos = service.VeiculoService.Carregar("modelo_id", modelo.getId() + "");
+                        tabela = (DefaultTableModel) this.telaBuscaVeiculo.getjTableDados().getModel();
+                        tabela.setRowCount(0);
+
+                        for (Veiculo veiculoAtualDaLista : listaVeiculos) {
+                            tabela.addRow(new Object[]{
+                                veiculoAtualDaLista.getId(),
+                                veiculoAtualDaLista.getPlaca(),
+                                veiculoAtualDaLista.getCor(),
+                                service.MarcaService.Carregar(modelo.getMarca().getId()).getDescricao(),
+                                modelo.getDescricao()
+                            });
+                        }
+                        break;
+                    case 4://Modelo
+                        List<Veiculo> listaVeiculos = new ArrayList<>();
+                        Modelo modelo = service.ModeloService.Carregar("descricao", this.telaBuscaVeiculo.getjTFFiltro().getText()).getFirst();
+
+                        listaVeiculos = service.VeiculoService.Carregar("modelo_id", modelo.getId() + "");
+                        tabela = (DefaultTableModel) this.telaBuscaVeiculo.getjTableDados().getModel();
+                        tabela.setRowCount(0);
+
+                        for (Veiculo veiculoAtualDaLista : listaVeiculos) {
+                            tabela.addRow(new Object[]{
+                                veiculoAtualDaLista.getId(),
+                                veiculoAtualDaLista.getPlaca(),
+                                veiculoAtualDaLista.getCor(),
+                                service.MarcaService.Carregar(modelo.getMarca().getId()).getDescricao(),
+                                modelo.getDescricao()
+                            });
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
-        //Botão Sair
+
+            //Botão Sair
         } else if (evento.getSource() == this.telaBuscaVeiculo.getjButtonSair()) {
             this.telaBuscaVeiculo.dispose();
         }
